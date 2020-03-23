4f5f429feab1b85e28b99417b61aefeee197c9c9
Zach Hoffman <zach@zrhoffman.net>
2020-03-23 09:14:00 -0600

    Update riak-cluster.sh to work with version 3.0

--- riak-cluster.sh
+++ riak-cluster.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,57 +12,58 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Cluster start script to bootstrap a Riak cluster.
+# Cluster start script to bootstrap a Riak 3.0 cluster.
 #
-set -ex
+set -o errexit -o nounset -o xtrace;
 
+set -o allexport;
 if [[ -x /usr/sbin/riak ]]; then
-  export RIAK=/usr/sbin/riak
+  RIAK=/usr/sbin/riak;
 else
-  export RIAK=$RIAK_HOME/bin/riak
-fi
-export RIAK_CONF=/etc/riak/riak.conf
-export USER_CONF=/etc/riak/user.conf
-export RIAK_ADVANCED_CONF=/etc/riak/advanced.config
-if [[ -x /usr/sbin/riak-admin ]]; then
-  export RIAK_ADMIN=/usr/sbin/riak-admin
-else
-  export RIAK_ADMIN=$RIAK_HOME/bin/riak-admin
-fi
-export SCHEMAS_DIR=/etc/riak/schemas/
+  RIAK=$RIAK_HOME/bin/riak;
+fi;
+RIAK_CONF=/etc/riak/riak.conf;
+USER_CONF=/etc/riak/user.conf;
+RIAK_ADVANCED_CONF=/etc/riak/advanced.config;
+RIAK_ADMIN="${RIAK} admin";
+SCHEMAS_DIR=/etc/riak/schemas/;
 
 # Set ports for PB and HTTP
-export PB_PORT=${PB_PORT:-8087}
-export HTTP_PORT=${HTTP_PORT:-8098}
+PB_PORT=${PB_PORT:-8087};
+HTTP_PORT=${HTTP_PORT:-8098};
 
 # Use ping to discover our HOSTNAME because it's easier and more reliable than other methods
-export HOST=$(ping -c1 $HOSTNAME | awk '/^PING/ {print $3}' | sed 's/[()]//g')||'127.0.0.1'
+HOST=$(ping -c1 $HOSTNAME | awk '/^PING/ {print $3}' | sed 's/[()]//g')||'127.0.0.1';
 
 # CLUSTER_NAME is used to name the nodes and is the value used in the distributed cookie
-export CLUSTER_NAME=${CLUSTER_NAME:-riak}
+CLUSTER_NAME=${CLUSTER_NAME:-riak};
 
 # The COORDINATOR_NODE is the first node in a cluster to which other nodes will eventually join
-export COORDINATOR_NODE=${COORDINATOR_NODE:-$HOSTNAME}
-export COORDINATOR_NODE_HOST=$(ping -c1 $COORDINATOR_NODE | awk '/^PING/ {print $3}' | sed 's/[()]//g')||'127.0.0.1'
+COORDINATOR_NODE=${COORDINATOR_NODE:-$HOSTNAME};
+COORDINATOR_NODE_HOST=$(ping -c1 $COORDINATOR_NODE | awk '/^PING/ {print $3}' | sed 's/[()]//g')||'127.0.0.1';
+set +o allexport;
 
 # Run all prestart scripts
-PRESTART=$(find /etc/riak/prestart.d -name *.sh -print | sort)
+PRESTART=$(find /etc/riak/prestart.d/ -name '*.sh' -print | sort);
 for s in $PRESTART; do
-  . $s
-done
+  source $s;
+done;
 
 # Start the node and wait until fully up
-$RIAK start
-$RIAK_ADMIN wait-for-service riak_kv
+$RIAK start;
+until $RIAK_ADMIN wait-for-service riak_kv; do
+  echo 'Waiting for riak_kv to start...';
+  sleep 2;
+done;
 
 # Run all poststart scripts
-POSTSTART=$(find /etc/riak/poststart.d -name *.sh -print | sort)
+POSTSTART=$(find /etc/riak/poststart.d/ -name '*.sh' -print | sort);
 for s in $POSTSTART; do
-  . $s
-done
+  source $s;
+done;
 
 # Trap SIGTERM and SIGINT and tail the log file indefinitely
-tail -n 1024 -f /var/log/riak/console.log &
-PID=$!
-trap "$RIAK stop; kill $PID" SIGTERM SIGINT
-wait $PID
+PID=$!;
+tail -F /var/log/riak/console.log &
+trap "$RIAK stop; kill $PID" SIGTERM SIGINT;
+wait $PID;
